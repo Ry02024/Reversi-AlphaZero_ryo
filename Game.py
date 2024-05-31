803500cff6be2d3b827e8c01f00433d3dfb99052
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2024-05-31 19:18:55 +0900

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,6 +1,10 @@
 import random
-import math
-
+import argparse
+import numpy as np
+from tensorflow.keras.models import load_model
+from datetime import datetime
+import os
+import json
 
 class State:
     #############################
@@ -16,18 +20,14 @@ class State:
         self.enemy_pieces = enemy_pieces
         self.depth = depth
 
-        if pieces == None or enemy_pieces == None:
+        if pieces is None or enemy_pieces is None:
             self.pieces = [0] * 64
             self.pieces[27] = self.pieces[36] = 1
             self.enemy_pieces = [0] * 64
             self.enemy_pieces[28] = self.enemy_pieces[35] = 1
 
     def piece_count(self, pieces):
-        count = 0
-        for i in pieces:
-            if i == 1:
-                count +=  1
-        return count
+        return sum(1 for i in pieces if i == 1)
 
     ##################################
     ####### ゲームの勝敗と終了判定 ########
@@ -37,10 +37,10 @@ class State:
         return self.piece_count(self.pieces) + self.piece_count(self.enemy_pieces) == 64 or self.pass_end
 
     def is_lose(self):
-        return self.done() and self.piece_count(self.pieces) < self.piece_count(self.enemy_pieces)
+        return self.is_done() and self.piece_count(self.pieces) < self.piece_count(self.enemy_pieces)
 
     def is_draw(self):
-        return self.done() and self.piece_count(self.pieces) == self.piece_count(self.enemy_pieces)
+        return self.is_done() and self.piece_count(self.pieces) == self.piece_count(self.enemy_pieces)
 
     ##########################
     ####### 合法手の判定 #######
@@ -51,38 +51,38 @@ class State:
         # 任意のマスの任意の方向が合法手か判定
         def legal_action_xy_dxy(x, y, dx, dy):
             # １つ目 相手の石
-            x, y = x+dx, y+dy
-            if y < 0 or 7 < y or x < 0 or 7 < x or self.enemy_pieces[x+y*8] != 1:
+            x, y = x + dx, y + dy
+            if y < 0 or 7 < y or x < 0 or 7 < x or self.enemy_pieces[x + y * 8] != 1:
                 return False
 
             # 2つ目以降
             for j in range(8):
                 # 空
-                if y < 0 or 7 < y or x < 0 or 7 < x or self.enemy_pieces[x+y*8] == 0 and self.pieces[x+y*8] == 0:
+                if y < 0 or 7 < y or x < 0 or 7 < x or (self.enemy_pieces[x + y * 8] == 0 and self.pieces[x + y * 8] == 0):
                     return False
 
                 # 自分の石
-                if self.pieces[x+y*8] == 1:
+                if self.pieces[x + y * 8] == 1:
                     # 反転
                     if flip:
                         for i in range(8):
-                            x, y = x-dx, y-dy
-                            if self.pieces[x+y*8] == 1:
+                            x, y = x - dx, y - dy
+                            if self.pieces[x + y * 8] == 1:
                                 return True
-                            self.pieces[x+y*8] = 1
-                            self.enemy_pieces[x+y*8] = 0
+                            self.pieces[x + y * 8] = 1
+                            self.enemy_pieces[x + y * 8] = 0
                     return True
                 # 相手の石
-                x, y = x+dx, y+dy
+                x, y = x + dx, y + dy
             return False
 
         # 空きなし
-        if self.enemy_pieces[x+y*8] == 1 or self.pieces[x+y*8] == 1:
+        if self.enemy_pieces[x + y * 8] == 1 or self.pieces[x + y * 8] == 1:
             return False
 
         # 石を置く
         if flip:
-            self.pieces[x+y*8] = 1
+            self.pieces[x + y * 8] = 1
 
         # 任意の位置が合法手かどうか
         flag = False
@@ -94,10 +94,10 @@ class State:
     # 合法手のリストの取得
     def legal_actions(self):
         actions = []
-        for j in range(0,8):
-            for i in range(0,8):
+        for j in range(8):
+            for i in range(8):
                 if self.legal_action_xy(i, j):
-                    actions.append(i+j*8)
+                    actions.append(i + j * 8)
         if len(actions) == 0:
             actions.append(64)
         return actions
@@ -107,13 +107,11 @@ class State:
     ##############################
 
     def next(self, action):
-        state = State(self.pieces.copy(), self.enemy_pieces.copy(), self.depth+1)
-        #挟んだ石を裏返す処理
+        state = State(self.pieces.copy(), self.enemy_pieces.copy(), self.depth + 1)
+        # 挟んだ石を裏返す処理
         if action != 64:
-            state.legal_action_xy(action%8, int(action/8), True)
-        w = state.pieces
-        state.pieces = state.enemy_pieces
-        state.enemy_pieces = w
+            state.legal_action_xy(action % 8, action // 8, True)
+        state.pieces, state.enemy_pieces = state.enemy_pieces, state.pieces
 
         # 2回連続パス判定
         if action == 64 and state.legal_actions() == [64]:
@@ -125,7 +123,7 @@ class State:
     ###########################
 
     def is_first_player(self):
-        return self.depth%2 == 0
+        return self.depth % 2 == 0
 
     #######################
     ####### 出力設定 #######
@@ -133,17 +131,18 @@ class State:
 
     def __str__(self):
         ox = ('o', 'x') if self.is_first_player() else ('x', 'o')
-        str = ''
+        board_str = ''
         for i in range(64):
             if self.pieces[i] == 1:
-                str += ox[0]
+                board_str += ox[0]
             elif self.enemy_pieces[i] == 1:
-                str += ox[1]
+                board_str += ox[1]
             else:
-                str += '-'
+                board_str += '-'
             if i % 8 == 7:
-                str += '\n'
-        return str
+                board_str += '\n'
+        return board_str
+
 
 #########################
 ######## 動作確認 ########
@@ -152,16 +151,86 @@ class State:
 # ランダムで行動選択
 def random_action(state):
     legal_actions = state.legal_actions()
-    return legal_actions[random.randint(0, len(legal_actions)-1)]
+    return legal_actions[random.randint(0, len(legal_actions) - 1)]
+
+# モデルで行動選択
+def model_action(state, model):
+    board_np = np.array(state.pieces + state.enemy_pieces).reshape((1, 8, 8, 2))
+    policies, _ = model.predict(board_np)
+    legal_actions = state.legal_actions()
+    legal_policies = [(policies[0][action], action) for action in legal_actions]
+    _, action = max(legal_policies)
+    return action
+
+def log_game_state(state, action, file):
+    # 機械学習用ログ
+    log_entry = {
+        "type": "machine",
+        "state": {
+            "pieces": state.pieces,
+            "enemy_pieces": state.enemy_pieces
+        },
+        "action": action
+    }
+    with open(file, 'a') as f:
+        f.write(json.dumps(log_entry) + "\n")
+    
+    # 人間向けログ
+    human_log_entry = {
+        "type": "human",
+        "action": action,
+        "board": str(state)
+    }
+    with open(file, 'a') as f:
+        f.write(json.dumps(human_log_entry) + "\n")
+
+def log_game_result(result, file):
+    with open(file, 'a') as f:
+        f.write(json.dumps({"type": "result", "result": result}) + "\n")
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use_model', action='store_true', help='Use model for action selection')
+    parser.add_argument('--model_path', type=str, default='path_to_your_model.h5', help='Path to the model file')
+    parser.add_argument('--log_file', type=str, default='game_log.json', help='Path to the log file')
+    args = parser.parse_args()
+
+    use_model = args.use_model
+    model = None
+
+    if use_model:
+        model = load_model(args.model_path)
+
+    log_file = args.log_file
+    
+    if log_file:
+        with open(log_file, 'w') as f:
+            f.write("")
+
     state = State()
 
     while True:
         if state.is_done():
             break
 
-        state = state.next(random_action(state))
+        if use_model and model:
+            action = model_action(state, model)
+        else:
+            action = random_action(state)
+
+        if log_file:
+            log_game_state(state, action, log_file)
+        
+        state = state.next(action)
 
         print(state)
         print()
+
+    if log_file:
+        if state.is_draw():
+            result = "Draw"
+        elif state.is_lose():
+            result = "Lose"
+        else:
+            result = "Win"
+        log_game_result(result, log_file)
