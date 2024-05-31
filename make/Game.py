4d12413000d5bfa86b623fbcf11bd427b4e44eef
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2024-05-31 09:57:54 +0900

    Update Game.py

--- make/Game.py
+++ make/Game.py
@@ -17,14 +17,42 @@ class State:
         return actions
 
     def is_legal(self, x, y):
-        # Implement legality check based on Reversi rules
-        pass
+        if self.board[x][y] != 0:
+            return False
+        for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < 8 and 0 <= ny < 8 and self.board[nx][ny] == -self.player:
+                while 0 <= nx < 8 and 0 <= ny < 8:
+                    nx += dx
+                    ny += dy
+                    if not (0 <= nx < 8 and 0 <= ny < 8):
+                        break
+                    if self.board[nx][ny] == self.player:
+                        return True
+                    if self.board[nx][ny] == 0:
+                        break
+        return False
 
     def next_state(self, action):
         new_state = State()
         new_state.board = [row[:] for row in self.board]
         new_state.player = -self.player
-        # Apply the action to the board and flip the opponent's pieces
+        x, y = action
+        new_state.board[x][y] = self.player
+        for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+            nx, ny = x + dx, y + dy
+            pieces_to_flip = []
+            while 0 <= nx < 8 and 0 <= ny < 8:
+                if new_state.board[nx][ny] == -self.player:
+                    pieces_to_flip.append((nx, ny))
+                elif new_state.board[nx][ny] == self.player:
+                    for fx, fy in pieces_to_flip:
+                        new_state.board[fx][fy] = self.player
+                    break
+                else:
+                    break
+                nx += dx
+                ny += dy
         return new_state
 
     def game_result(self):
