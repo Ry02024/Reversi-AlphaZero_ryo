a5ee99c262aa38445057859d363bb8895129b59a
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2024-06-03 10:08:22 +0900

    Update EvaluateNetwork.py

--- EvaluateNetwork.py
+++ EvaluateNetwork.py
@@ -5,6 +5,7 @@ from tensorflow.keras import backend as K
 from pathlib import Path
 from shutil import copy
 import numpy as np
+import matplotlib.pyplot as plt
 
 EN_GAME_COUNT = 1 # 1評価あたりのゲーム数
 EN_TEMPERATURE = 1.0 # ボルツマン分布の温度
@@ -35,6 +36,17 @@ def update_best_player():
     copy('./model/latest.h5', './model/best.h5')
     print('Change BestPlayer')
 
+def plot_evaluation_results(results):
+    games = range(1, len(results) + 1)
+    points = [result['points'] for result in results]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(games, points, 'bo-', label='Points per game')
+    plt.title('Evaluation Results')
+    plt.xlabel('Game')
+    plt.ylabel('Points')
+    plt.legend()
+    plt.show()
 
 def evaluate_network():
     model0 = load_model('./model/latest.h5')
@@ -47,11 +59,16 @@ def evaluate_network():
 
 
     total_point = 0
+    evaluation_results = []
     for i in range(EN_GAME_COUNT):
         if i % 2 == 0:
-            total_point += play(next_actions)
+            points = play(next_actions)
+            total_point += points
         else:
-            total_point += 1 - play(list(reversed(next_actions)))
+            points = 1 - play(list(reversed(next_actions)))
+            total_point += points
+
+        evaluation_results.append({'game': i + 1, 'points': points})
 
         print('\rEvaluate {}/{}'.format(i + 1, EN_GAME_COUNT), end='')
     print('')
@@ -63,6 +80,7 @@ def evaluate_network():
     del model0
     del model1
 
+    plot_evaluation_results(evaluation_results)
 
     if average_point > 0.5:
         update_best_player()
